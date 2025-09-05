15ea31bac71f2c8cbb87c0efc33b1f5f339e5bcd
Oleksandr Rudyk <oleksandr_rudyk@epam.com>
2025-09-05 11:23:44 +0300

    lesson 19 code added

--- core/api/swapi/swapi_controller.py
+++ core/api/swapi/swapi_controller.py
@@ -1,22 +1,16 @@
-import requests
-import logging
-
 from core.api.rest_base import RestBase
-
-logger = logging.getLogger("swapi_controller")
-
 
 class SwapiController(RestBase):
     def __init__(self, url="https://swapi.info/api"):
         self.url = url
 
-    def get_person(self, user_id):
+    def get_person(self, user_id, status_code=200):
         url = f"{self.url}/people/{user_id}"
-        return self._execute_request(method="get", url=url)
+        return self._execute_request(method="get", url=url, status_code=status_code)
 
-    def get_planet(self, planet_id):
+    def get_planet(self, planet_id, status_code=200):
         url = f"{self.url}/planet/{planet_id}"
-        return self._execute_request(method="get", url=url)
+        return self._execute_request(method="get", url=url, status_code=status_code)
 
 
     def get_people(self):
--- tests/api_tests/swapi_tests/test_get_user.py
+++ tests/api_tests/swapi_tests/test_get_user.py
@@ -4,14 +4,15 @@ from core.api.swapi.swapi_controller import SwapiController
 
 
 
-@pytest.mark.swapi
-@pytest.mark.parametrize("user_id", [1, 2, 3], ids=["user_id_1", "user_id_2", "user_id_3"])
-def test_get_user(user_id):
-    response = SwapiController().get_person(user_id)
-    assert response.text is not None
+# @pytest.mark.swapi
+# @pytest.mark.parametrize("user_id", [1, 2, 0], ids=["user_id_1", "user_id_2", "user_id_3"])
+# def test_get_user(user_id):
+#     response = SwapiController().get_person(user_id)
+#     print(response.text)
+#     assert response.text is not None
 
 @pytest.mark.swapi
-@pytest.mark.parametrize("user_id", [1, 2, 3], ids=["user_id_1", "user_id_2", "user_id_3"])
+@pytest.mark.parametrize("user_id", [1, 2, 500], ids=["user_id_1", "user_id_2", "user_id_3"])
 def test_get_user(user_id):
     response = SwapiController().get_person(user_id)
     assert response.text is not None
