91a9a3a103afd693cb4e67ed41183d221fd9e278
Oleksandr Rudyk <oleksandr_rudyk@epam.com>
2025-10-24 14:51:04 +0300

    add chrome options

--- tests/ui_tests/saucedemo/conftest.py
+++ tests/ui_tests/saucedemo/conftest.py
@@ -11,6 +11,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     chrome_options = ChromiumOptions()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(options=chrome_options, service=service)
     # driver = Chrome()
